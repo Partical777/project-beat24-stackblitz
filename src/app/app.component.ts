e2ec6a71d77c344955a251c21aa81935269ea9f1
Partical777 <z25993021@gmail.com>
2019-09-18 17:33:07 +0800

    GSAP + Scrollmagic Finally Work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,21 +13,29 @@ export class AppComponent  {
   logo1;
   scene;
 
-  ngOnInit() {
+  ngAfterViewInit() {
     this.controller = new ScrollMagic.Controller();
   
-    this.logo1 = new TimelineMax({repeat:-1, repeatDelay:1});
+    this.logo1 = new TimelineMax({onUpdate: this.updatePercentage});
 
     this.logo1.from(".test2", 1, {x: 1500, opacity: 0});
 
-    this.scene = new ScrollMagic({
-        triggerElement : "sec2",
+    new ScrollMagic.Scene({
+        triggerElement : ".sec2",
         triggerHook : "onLeave",
         duration: "100%"
       })
-        .setPin("sec2")
+        .setPin(".sec2")
         .setTween(this.logo1)
         .addTo(this.controller);
+
+        console.log(this.controller);
+  }
+
+  updatePercentage() {
+  //percent.innerHTML = (tl.progress() *100 ).toFixed();
+    this.logo1.progress();
+    console.log(this.logo1.progress());
   }
 
   
